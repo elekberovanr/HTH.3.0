9c73b694ba30edcdf9f33c44cfe05520bab3c684
elekberovanr <nurelekberova2005@icloud.com>
2025-07-04 15:24:39 +0400

    Home dark mode

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,25 +1,35 @@
-import React from 'react'
-import SearchAndCategories from './searchAndCategories/SearchAndCategories'
-import Products from './products/Products'
-import SupportButton from '../supportButton/SupportButton'
-import HeroSection from './heroSection/HeroSection'
-import WhatsNew from './whatsNew/WhatsNew'
-import CategoriesSection from './categoriesSection/CateoriesSection'
-import UsersSection from './userSection/UsersSection'
-
+import React, { useEffect } from 'react';
+import styles from './Home.module.css';
+import SearchAndCategories from './searchAndCategories/SearchAndCategories';
+import Products from './products/Products';
+import SupportButton from '../supportButton/SupportButton';
+import HeroSection from './heroSection/HeroSection';
+import WhatsNew from './whatsNew/WhatsNew';
+import CategoriesSection from './categoriesSection/CateoriesSection';
+import UsersSection from './userSection/UsersSection';
 
 const Home = () => {
+  useEffect(() => {
+    const theme = localStorage.getItem('theme') || 'light';
+    document.body.setAttribute('data-theme', theme);
+  }, []);
+
   return (
-    <div>
-      <HeroSection/>
-      <WhatsNew/>
-      <SupportButton/>
-      <CategoriesSection onCategorySelect={(categoryName) => setSearchTerm(categoryName)} />
-      <UsersSection/>
-      <Products/>
-
+    <div className={styles.homeContainer}>
+      <HeroSection />
+      <WhatsNew />
+      <SupportButton />
+      <div className={styles.sectionSpacing}>
+        <CategoriesSection onCategorySelect={(categoryName) => setSearchTerm(categoryName)} />
+      </div>
+      <div className={styles.sectionSpacing}>
+        <UsersSection />
+      </div>
+      <div className={styles.sectionSpacing}>
+        <Products />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
